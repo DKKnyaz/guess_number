82a5c7dacfd3d427f92906b71d72f3d4a6c84c83
Knyazev Danil <DKKnyaz@yandex.ru>
2024-12-09 20:21:39 +1000

    Add file guess_number

---
+++ guess_number.py
@@ -0,0 +1,19 @@
+from random import randint
+
+
+hidden_number = randint(1, 100)
+print('Угдайте число от 1 до 100')
+    
+
+while True:
+    guess = int(input('Введите число: '))
+    
+    if hidden_number > guess:
+        print('Ваше число меньше того, что загадано')
+    
+    if hidden_number < guess:
+        print('Ваше число больше того, что загадано')
+    
+    if hidden_number == guess:
+        break    
+print('Отличная интуиция! Вы угадали число :)')
